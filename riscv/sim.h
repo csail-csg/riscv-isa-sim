b929a897a62b4915f96578e9c33fb9a4507ac22c
acw1251 <acwright@mit.edu>
2015-11-17 17:28:24 -0500

    Modify INTERLEAVE and INSNS_PER_RTC_TICK to behave better with tandem verif

--- riscv/sim.h
+++ riscv/sim.h
@@ -52,8 +52,8 @@ private:
   processor_t* get_core(const std::string& i);
   void step(size_t n); // step through simulation
  private:
-  static const size_t INTERLEAVE = 5000;
-  static const size_t INSNS_PER_RTC_TICK = 100; // 10 MHz clock for 1 BIPS core
+  static const size_t INTERLEAVE = 128;
+  static const size_t INSNS_PER_RTC_TICK = 128; // not a 10 MHz clock for 1 BIPS core
   reg_t rtc;
   size_t current_step;
   size_t current_proc;
